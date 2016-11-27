fd4846597657c98dbcf796fe3ad4eb01222c12e6
Mª Asuncion <M Asuncion@192.168.1.3>
2016-11-27 22:24:13 +0100

    En proceso

---
+++ MarioBross/src/JLabelTuberia.java
@@ -0,0 +1,67 @@
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class JLabelTuberia extends JLabel {
+	private static final long serialVersionUID = 1L; // Para serialización
+
+	public static final int TAMANYO_BLOQUE = 200; // píxels (igual ancho que
+													// largo)
+
+	public static final int ARISTA_RECTANGULO_BLOQUE = 199; // Radio en píxels
+															// del bounding
+															// circle del
+															// Mario(para
+															// choques)
+
+	private static final boolean DIBUJAR_RECTANGULO_BLOQUE = true; // Dibujado
+																	// (paradepuración)del
+																	// bounding
+																	// circle de
+																	// choque de
+																	// Mario
+
+	/**
+	 * Construye y devuelve el JLabel del Bloque con su gráfico y tamaño
+	 */
+
+	public JLabelTuberia() {
+		try {
+
+			setIcon(new ImageIcon(JLabelMario.class.getResource("Imagenes/TuboMario.png").toURI().toURL()));
+		} catch (Exception e) {
+			System.err.println("Error en carga de recurso: Mario.png no encontrado");
+			e.printStackTrace();
+		}
+		setBounds(0, 0, TAMANYO_BLOQUE, 350);
+
+	}
+
+	/**
+	 * Método para pintar el componente de Mario
+	 */
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// super.paintComponent(g); // En este caso no nos sirve el pintado
+		// normal de un JLabel
+		Image img = ((ImageIcon) getIcon()).getImage();
+		Graphics2D g2 = (Graphics2D) g; // El Graphics realmente es Graphics2D
+		// Escalado más fino con estos 3 parámetros:
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		// Prepara rotación (siguientes operaciones se rotarán)
+		// Dibujado de la imagen
+		g2.drawImage(img, 0, 10, TAMANYO_BLOQUE, 350, null);
+
+		if (DIBUJAR_RECTANGULO_BLOQUE)
+			g2.drawRect(0, 10, ARISTA_RECTANGULO_BLOQUE, ARISTA_RECTANGULO_BLOQUE+150);
+	}
+
+}
